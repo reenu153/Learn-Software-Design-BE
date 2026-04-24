0e91f2ba7eb0cc73e7f7b1496b53a29163af5030
Reenu <reenumariajerry@gmail.com>
2026-04-24 15:55:35 +0200

    add allowed origins

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.database import engine, Base
 from app.routers import course, q, questions, login,module 
+import os
 
 app = FastAPI()
 
