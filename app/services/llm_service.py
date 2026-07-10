c23f6fe50dbb0e385030f80a66ca69892b378b1d
Reenu <reenumariajerry@gmail.com>
2026-07-10 11:51:36 +0200

    change gpt model

--- app/services/llm_service.py
+++ app/services/llm_service.py
@@ -13,12 +13,12 @@ def evaluate_solution(question: str, solution: str, criteria: str):
     prompt = prompt_without_crieria.format(question=question, solution=solution, criteria=criteria)
 
     response = client.chat.completions.create(
-        model="gpt-4o-mini",
+        model="gpt-5",
         messages=[
             {"role": "system", "content": "You are an expert software design evaluator."},
             {"role": "user", "content": prompt}
         ],
-        temperature=0.2,
+        # temperature=0.2,
         response_format={"type": "json_object"},
     )
     result = json.loads(response.choices[0].message.content)
